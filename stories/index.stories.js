861b4f1a3ce0246d4a3264ccbe515ac7f86eb45c
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2019-04-23 15:07:16 -0300

    Deleted unnecessary index.js for stories

--- stories/index.stories.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
-
-const req = require.context('./components', true, /\.stories\.js$/);
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-/* eslint-enable react/react-in-jsx-scope */
